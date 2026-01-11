e19c4c31cf9529b7eac004b6ee333b3b61a0d329
Trilok Saini <triloksaini65@gmail.com>
2026-01-11 05:50:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5165676555-45ce5",
+  "appId": "1:222136851356:web:edae6ef71657b8eef0c659",
+  "apiKey": "AIzaSyDSfqXD9-Mz2F1rWMqABnIshmBOQrJOYRY",
+  "authDomain": "studio-5165676555-45ce5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "222136851356"
+};
